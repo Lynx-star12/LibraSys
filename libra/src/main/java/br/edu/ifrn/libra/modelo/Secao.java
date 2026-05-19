ca8762119bec694922ad21af124f9891c1fda557
Adi Hilary <adihillarys.o0@gmail.com>
2026-05-19 15:44:47 -0300

    adicionando requisito 001

--- libra/src/main/java/br/edu/ifrn/libra/modelo/Secao.java
+++ libra/src/main/java/br/edu/ifrn/libra/modelo/Secao.java
@@ -2,7 +2,33 @@ package br.edu.ifrn.libra.modelo;
 
 public class Secao {
 
-    private String genero;
+    private String nomeGenero;
+    private String codigo;
+    private String localizacao;
 
-    
+
+
+    public String getNomeGenero() {
+        return nomeGenero;
+    }
+
+    public void setNomeGenero(String nomeGenero) {
+        this.nomeGenero = nomeGenero;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getLocalizacao() {
+        return localizacao;
+    }
+
+    public void setLocalizacao(String localizacao) {
+        this.localizacao = localizacao;
+    }
 }
