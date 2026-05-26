db09465b080a17f3c8d71c20af9a6e4a7a98eb98
Adi Hillary <oliveira.hillary@escolar.ifrn.edu.br>
2026-05-26 15:52:38 -0300

    criando as classes usuario modelo e usuario servico

--- libra/src/main/java/br/edu/ifrn/libra/modelo/usuario.java
+++ libra/src/main/java/br/edu/ifrn/libra/modelo/usuario.java
@@ -2,4 +2,57 @@ package br.edu.ifrn.libra.modelo;
 
 public class usuario {
 
+    private String nomeCompleto;
+    private String cpf;
+    private String email;
+    private String telefone;
+    private String statusCadastro;
+
+    // Método para verificar se usuário está ativo
+    public boolean isAtivo() {
+
+        return statusCadastro.equalsIgnoreCase("ATIVO");
+    }
+
+    // GETTERS E SETTERS
+
+    public String getNomeCompleto() {
+        return nomeCompleto;
+    }
+
+    public void setNomeCompleto(String nomeCompleto) {
+        this.nomeCompleto = nomeCompleto;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getStatusCadastro() {
+        return statusCadastro;
+    }
+
+    public void setStatusCadastro(String statusCadastro) {
+        this.statusCadastro = statusCadastro;
+    }
 }
---
+++ libra/src/main/java/br/edu/ifrn/libra/usuarioServico.java
@@ -0,0 +1,5 @@
+package br.edu.ifrn.libra;
+
+public class usuarioServico {
+
+}
