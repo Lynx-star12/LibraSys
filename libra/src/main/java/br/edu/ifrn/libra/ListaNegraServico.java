d4c730fc0f2bae7f8fad13ebb3a0f2be3d965247
Adi Hillary <oliveira.hillary@escolar.ifrn.edu.br>
2026-05-26 16:25:08 -0300

    criando as classes do req 004 e adicionando o esqueleto dos codigos

---
+++ libra/src/main/java/br/edu/ifrn/libra/ListaNegraServico.java
@@ -0,0 +1,29 @@
+package br.edu.ifrn.libra;
+
+import br.edu.ifrn.libra.modelo.ListaNegra;
+public class ListaNegraServico {
+
+    public void verificarPendencias(
+        ListaNegra usuario) {
+
+    // Se possui multa ou atraso → bloqueia
+
+    if (usuario.isPossuiMulta()
+            || usuario.isPossuiAtraso()) {
+
+        usuario.setBloqueado(true);
+
+        System.out.println(
+                "Usuário bloqueado!"
+        );
+
+    } else {
+
+        usuario.setBloqueado(false);
+
+        System.out.println(
+                "Usuário liberado!"
+        );
+    }
+}
+}
---
+++ libra/src/main/java/br/edu/ifrn/libra/modelo/ListaNegra.java
@@ -0,0 +1,43 @@
+package br.edu.ifrn.libra.modelo;
+
+public class ListaNegra {
+
+    private String cpfUsuario;
+    private boolean possuiMulta;
+    private boolean possuiAtraso;
+    private boolean bloqueado;
+
+    // GETTERS E SETTERS
+
+    public String getCpfUsuario() {
+        return cpfUsuario;
+    }
+
+    public void setCpfUsuario(String cpfUsuario) {
+        this.cpfUsuario = cpfUsuario;
+    }
+
+    public boolean isPossuiMulta() {
+        return possuiMulta;
+    }
+
+    public void setPossuiMulta(boolean possuiMulta) {
+        this.possuiMulta = possuiMulta;
+    }
+
+    public boolean isPossuiAtraso() {
+        return possuiAtraso;
+    }
+
+    public void setPossuiAtraso(boolean possuiAtraso) {
+        this.possuiAtraso = possuiAtraso;
+    }
+
+    public boolean isBloqueado() {
+        return bloqueado;
+    }
+
+    public void setBloqueado(boolean bloqueado) {
+        this.bloqueado = bloqueado;
+    }
+}
