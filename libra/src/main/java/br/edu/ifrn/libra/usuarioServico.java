d92f19313c1a067bd64fc19650cec3f05c68c916
Adi Hillary <oliveira.hillary@escolar.ifrn.edu.br>
2026-05-26 15:57:04 -0300

    adicionando esqueleto do codigo  usuarioServico

--- libra/src/main/java/br/edu/ifrn/libra/usuarioServico.java
+++ libra/src/main/java/br/edu/ifrn/libra/usuarioServico.java
@@ -1,5 +1,69 @@
 package br.edu.ifrn.libra;
 
+import br.edu.ifrn.libra.modelo.usuario;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class usuarioServico {
 
+    private List<usuario> usuarios = new ArrayList<>();
+
+    public void cadastrarUsuario(usuario novoUsuario) {
+
+        // Validar nome
+        if (novoUsuario.getNomeCompleto() == null
+                || novoUsuario.getNomeCompleto().isEmpty()) {
+
+            throw new IllegalArgumentException(
+                    "O nome completo é obrigatório."
+            );
+        }
+
+        // Validar CPF
+        if (novoUsuario.getCpf() == null
+                || novoUsuario.getCpf().isEmpty()) {
+
+            throw new IllegalArgumentException(
+                    "O CPF é obrigatório."
+            );
+        }
+
+        // Validar Email
+        if (novoUsuario.getEmail() == null
+                || novoUsuario.getEmail().isEmpty()) {
+
+            throw new IllegalArgumentException(
+                    "O e-mail é obrigatório."
+            );
+        }
+
+        // Validar Telefone
+        if (novoUsuario.getTelefone() == null
+                || novoUsuario.getTelefone().isEmpty()) {
+
+            throw new IllegalArgumentException(
+                    "O telefone é obrigatório."
+            );
+        }
+
+        // Verificar CPF duplicado
+        for (usuario usuario : usuarios) {
+
+            if (usuario.getCpf().equals(novoUsuario.getCpf())) {
+
+                throw new IllegalArgumentException(
+                        "CPF já cadastrado no sistema."
+                );
+            }
+        }
+
+        // Salvar usuário
+        usuarios.add(novoUsuario);
+
+        System.out.println(
+                "Usuário cadastrado com sucesso!"
+        );
+    }
 }
